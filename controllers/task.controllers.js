92865091549f59778b7124cb14d548825c0e0a10
Vikash Gupta <vikashg2512@gmail.com>
2023-04-02 12:15:11 +0530

    tasks crud api's creation

--- controllers/task.controllers.js
+++ controllers/task.controllers.js
@@ -1,5 +1,57 @@
-import { User } from "../models/users.models.js";
-import bcrypt from "bcrypt";
-import { sendCookies } from "../utils/features.js";
+import { Task } from "../models/tasks.models.js";
 
-export const newTask = async (req, res) => {};
+export const newTask = async (req, res, next) => {
+  const { title, description } = req.body;
+  //   const task = new Task({title})
+  //   await task.save()
+
+  await Task.create({
+    title,
+    description,
+    user: req.user,
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "Task added sucessfully",
+  });
+};
+
+export const getAllTask = async (req, res, next) => {
+  const userid = req.user._id;
+
+  const tasks = await Task.find({ user: userid });
+
+  res.status(200).json({
+    success: true,
+    tasks,
+  });
+};
+
+export const updateTask = async (req, res, next) => {
+  const task = await Task.findById(req.params.id);
+  if (!task)
+    return res.status(404).json({ success: false, message: "Task not found" });
+  task.isCompleted = !task.isCompleted;
+
+  await task.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Task updated",
+  });
+};
+
+export const deleteTask = async (req, res, next) => {
+  const task = await Task.findById(req.params.id);
+
+  if (!task)
+    return res.status(404).json({ success: false, message: "Task not found" });
+
+  await task.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "Task deleted",
+  });
+};
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -2,10 +2,21 @@ import express from "express";
 import { User } from "../models/users.models.js";
 
 import { isAuthenticated } from "../middlewares/auth.js";
-import { newTask } from "../controllers/task.controllers.js";
+import {
+  deleteTask,
+  getAllTask,
+  newTask,
+  updateTask,
+} from "../controllers/task.controllers.js";
 
 const router = express.Router();
 
-router.post("/new", newTask);
+router.post("/new", isAuthenticated, newTask);
+router.get("/all", isAuthenticated, getAllTask);
+
+router
+  .route("/:id")
+  .put(isAuthenticated, updateTask)
+  .delete(isAuthenticated, deleteTask);
 
 export default router;
