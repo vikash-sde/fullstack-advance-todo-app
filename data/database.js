7a7f90126a401298dc9307c864508cc39404ae9d
Vikash Gupta <vikashg2512@gmail.com>
2023-04-02 16:47:56 +0530

    db url

--- data/database.js
+++ data/database.js
@@ -1,11 +1,8 @@
 import { mongoose } from "mongoose";
 
-const CONNECTION_URL =
-  "mongodb+srv://user:user@socialmedia.nwmyj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
 export const connectDB = () => {
   mongoose
     .connect(process.env.MOONGO_URL, { dbName: "backend" })
-    .then(() => console.log("database connected"))
+    .then((c) => console.log(`database connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
